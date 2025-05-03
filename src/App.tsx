ebef794f69e90fa2220e3451aa052555bc133cc0
thientq52 <thien.tran0502@hcmut.edu.vn>
2025-05-03 16:41:16 +0000

    articles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { AuthProvider } from "./common/AuthProvider.tsx";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Login from "./pages/Login.tsx";
-
+import ArticleList from './pages/ArticleList';
+import ArticleDetail from './pages/ArticleDetail';
+import ArticleEdit from './pages/ArticleEdit';
 import TopBar from "./common/TopBar.tsx";
 import Footer from "./common/Footer.tsx";
 import "./App.css";
@@ -25,7 +27,9 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-
+          <Route path="/articles" element={<ArticleList />} />
+          <Route path="/articles/:id" element={<ArticleDetail />} />
+          <Route path="/articles/edit/:id" element={<ArticleEdit />} />
           <Route path="/qa" element={<QnAList />} />
           <Route path="/qa/:threadId" element={<QnAThread />} />
         </Routes>
